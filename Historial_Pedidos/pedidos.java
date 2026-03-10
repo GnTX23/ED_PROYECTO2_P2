11b7e9dfde0cc07cd4895c542d29864ada3aeb27
MiguelCruz-tech <utm24090543@utma.edu.mx>
2026-03-10 03:04:27 +0000

    PruebaMiguel

---
+++ Historial_Pedidos/pedidos.java
@@ -0,0 +1,15 @@
+ /*3. Historial de Pedidos Recientes
+El sistema debe registrar los últimos pedidos procesados en cada caja utilizando una pila. Esto permite al
+cajero deshacer el último pedido en caso de error, operación por operación. Las operaciones básicas con
+pilas incluyen: consultar el pedido en el tope, verificar si la pila está vacía antes de extraer, e insertar
+nuevos pedidos, considerando el caso especial de inserción en una pila vacía al iniciar el turno de cada
+cajero. */
+ public class pedidos{
+ public static void main (String args[]){
+    byte max=9, op=0;
+    int tope=-1, i=0;
+
+    byte v[] = new byte[10];
+    
+ }
+ }
